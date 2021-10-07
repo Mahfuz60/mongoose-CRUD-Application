87ff65d505b36a99b5178eb41fe9fb5ede96fba6
mahfuz60 <mahfuzuralam1996@gmail.com>
2021-10-08 02:09:14 +0600

    code

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ app.use(express.json());
 //database connection with mongoose
 mongoose
   .connect("mongodb://localhost/todo")
-  .then(() => console.log("connection successfully done!"))
+  .then(() => console.log("DB connection successfully done!"))
   .catch((err) => console.log(err));
 
-//application routes
+//application routers
 app.use("/todo", todoHandler);
+
 //default error handler
 const errorHandler = (err, req, res, next) => {
   if (res.handlersSent) {
--- routerHandler/todoHandler.js
+++ routerHandler/todoHandler.js
@@ -1,31 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const todoSchema = require("../Schema/todoSchema");
 
 //GET all the TODO
-router.get("/", async (req, res) => {
-
-});
+router.get("/", async (req, res) => {});
 //GET A TODO by ID
-router.get("/:id", async (req, res) => {
-
-});
+router.get("/:id", async (req, res) => {});
 //POST TODO
-router.post("/", async (req, res) => {
-
-});
+router.post("/", async (req, res) => {});
 //POST multiple TODO
-router.post("/multiple", async (req, res) => {
-
-});
+router.post("/multiple", async (req, res) => {});
 //PUT  TODO(update data)
-router.put("/:id", async (req, res) => {
-
-});
+router.put("/:id", async (req, res) => {});
 
 //DELETE TODO(delete data)
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {});
 
-});
-
-//export router 
-module.exports=router;
+//export router
+module.exports = router;
